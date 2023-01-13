355fbcd59d46dc801a36e8b7bc0a6f8f2d6b3f04
ListOrPad <ListOrPad@gmail.com>
2023-01-13 22:53:41 +0300

    123

---
+++ Studying/CSharp Book Basics/Consolas and Telim/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Consolas_and_Telim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bread is ready");
+            Console.WriteLine("Who is the bread for?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Noted: {name} got bread");
+        }
+    }
+}
