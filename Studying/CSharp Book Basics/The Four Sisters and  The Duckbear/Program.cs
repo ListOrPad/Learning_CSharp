30885125a5659067bb13f4fc5d09f569f9ad599f
ListOrPad <ListOrPad@gmail.com>
2023-01-15 18:11:57 +0300

    created input method

---
+++ Studying/CSharp Book Basics/The Four Sisters and  The Duckbear/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace The_Four_Sisters_and__The_Duckbear
+{
+    internal class Program
+    {
+        static string Input(string text)
+        {
+            Console.WriteLine(text);
+            string input = Console.ReadLine();
+            return input;
+        }
+        static void Main(string[] args)
+        {
+            string x = Input("how old are you?");
+            Console.WriteLine($"{x} and you?");
+        }
+    }
+}
