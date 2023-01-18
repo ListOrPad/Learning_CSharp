2ac8617d6b0f608ff8fd08aa3ef3ee7a29c56bd1
ListOrPad <ListOrPad@gmail.com>
2023-01-18 18:25:14 +0300

    very small exercise on "if" statements

---
+++ Studying/CSharp Book Basics/small decision making task/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace small_decision_making_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your number? ");
+            int ticking = Convert.ToInt32(Console.ReadLine());
+            if (ticking % 2 == 0)
+                Console.WriteLine("tick");
+            else
+                Console.WriteLine("tock");
+        }
+    }
+}
