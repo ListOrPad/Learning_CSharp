e49b79494cee3aed9f9c5908792cb877be38480b
ListOrPad <112974861+ListOrPad@users.noreply.github.com>
2023-01-20 20:12:20 +0300

    Solution enhanced

---
+++ Studying/CSharp Book Basics/Switches book task/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Switches_book_task
+{
+    internal class Program
+    {
+        static string InputNoLine(string text)
+        {
+            Console.Write(text);
+            string input = Console.ReadLine();
+            return input;
+        }
+        static void ShowItemPrice(int choice)
+        {
+
+            string item = choice switch
+            {
+                1 => "Rope",
+                2 => "Torches",
+                3 => "Climbing Equipment",
+                4 => "Clean Water",
+                5 => "Machete",
+                6 => "Canoe",
+                7 => "Food supplies"
+            };
+
+            int price = item switch
+            {
+                "Rope" => 10,
+                "Torches" => 15,
+                "Climbing Equipment" => 25,
+                "Clean Water" => 1,
+                "Machete" => 20,
+                "Canoe" => 200,
+                "Food supplies" => 1
+            };
+
+
+            Console.WriteLine($"{item} cost {price} gold");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The following Items are available:\n" +
+                "1 - Rope\n" +
+                "2 - Torches\n" +
+                "3 - Climbing Equipment\n" +
+                "4 - Clean Water\n" +
+                "5 - Machete\n" +
+                "6 - Canoe\n" +
+                "7 - Food supplies");
+            int choice = Convert.ToInt32(InputNoLine("What item do you want to see the price of?"));
+            ShowItemPrice(choice);
+
+        }
+    }
+}
--- Studying/CSharp Book Basics/The Watchtower Challenge/Program.cs
+++ Studying/CSharp Book Basics/The Watchtower Challenge/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+        static string Input(string text)
+        {
+            Console.WriteLine(text);
+            string input = Console.ReadLine();
+            return input;
+        }
         static void Main(string[] args)
         {
+            int x = Convert.ToInt32(Input("tell the x coordinate"));
+            int y = Convert.ToInt32(Input("tell the y coordinate"));
 
+           
+            if (x < 0 && y > 0) Console.WriteLine("NW");
+            if (x == 0 && y > 0) Console.WriteLine("N");
+            if (x > 0 && y > 0) Console.WriteLine("NE");
+            if (x < 0 && y == 0) Console.WriteLine("W");
+            if (x == 0 && y == 0) Console.WriteLine("Right Here!");
+            if (x > 0 && y == 0) Console.WriteLine("E");
+            if (x < 0 && y < 0) Console.WriteLine("SW");
+            if (x == 0 && y < 0) Console.WriteLine("S");
+            if (x > 0 && y < 0) Console.WriteLine("SE");
         }
     }
 }
