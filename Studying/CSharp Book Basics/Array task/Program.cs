07a63e1ac5c88888c7229f56aa04aa1491a0e685
ListOrPad <112974861+ListOrPad@users.noreply.github.com>
2023-01-24 19:36:35 +0300

    little bit modified first array task from cSharp player book

---
+++ Studying/CSharp Book Basics/Array task/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Array_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //First array creation, taking numbers
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                string[] numberTaken = new string[5] { "first", "second", "third", "fourth", "fifth" };
+                Console.WriteLine($"Enter the {numberTaken[i]} number"); 
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //Making copy
+            int[] arrayCopy = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayCopy[i] = array[i];
+                Console.WriteLine($"First array: {array[i]}, Second array: {arrayCopy[i]}");
+            }
+        }
+    }
+}
