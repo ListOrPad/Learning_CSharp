1fdf4476f40b420f1174eb29425811b578987bd0
ListOrPad <ListOrPad@gmail.com>
2023-02-20 15:23:49 +0300

    Exercise 3 before looking into the solution

---
+++ Studying/CSharp Book OOP/24 Task 3/Card.cs
@@ -0,0 +1,42 @@
+﻿public class Card
+{
+    public CardRank Rank { get; set; }
+    public CardColor Color { get; set; }
+
+    public CardRank One { get; } = CardRank.One;
+    public CardRank Two { get; } = CardRank.Two;
+    public CardRank Three { get; } = CardRank.Three;
+    public CardRank Four { get; } = CardRank.Four;
+    public CardRank Five { get; } = CardRank.Five;
+    public CardRank Six { get; } = CardRank.Six;
+    public CardRank Seven { get; } = CardRank.Seven;
+    public CardRank Eight { get; } = CardRank.Eight;
+    public CardRank Nine { get; } = CardRank.Nine;
+    public CardRank Ten { get; } = CardRank.Ten;
+    public CardRank Dollar { get; } = CardRank.Dollar;
+    public CardRank Percent { get; } = CardRank.Percent;
+    public CardRank Caret { get; } = CardRank.Caret;
+    public CardRank Ampersand { get; } = CardRank.Ampersand;
+
+    public CardColor Red { get; } = CardColor.Red;
+    public CardColor Blue { get; } = CardColor.Blue;
+    public CardColor Green { get; } = CardColor.Green;
+    public CardColor Yellow { get; } = CardColor.Yellow;
+
+    public CardRank[] GetRanks() 
+    {
+        return new[] { One, Two, Three, Four, Five, Six, Seven, Eight,
+                        Nine, Ten, Dollar, Percent, Caret, Ampersand };
+    }
+    public CardColor[] GetColors()
+    {
+        return new[] { Red, Blue, Green, Yellow };
+    }
+
+    public Card() { }
+    public Card(CardRank rank, CardColor color)
+    {
+        Rank = rank;
+        Color = color;
+    }
+}
---
+++ Studying/CSharp Book OOP/24 Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace _24_Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prepare ranks and colors for the card object
+            Card cardInit = new Card();
+            CardRank[] ranks = cardInit.GetRanks();
+            CardColor[] colors = cardInit.GetColors();
+
+
+            for (int i = 0; i < 14; i++)        //14 == 14 ranks in a deck
+            {
+                for (int j = 0; j < 4; j++)         //4 because 4 colors for each rank in a deck
+                {
+                    Card card = new Card(ranks[i], colors[j]);
+                    Console.WriteLine($"The {card.Color} {card.Rank}");
+                }
+            }
+        }
+    }
+}
