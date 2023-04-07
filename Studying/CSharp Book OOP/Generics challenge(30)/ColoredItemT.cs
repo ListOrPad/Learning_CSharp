cb52f85ac44aeb3c3d21311f9b0349be7bed5c43
ListOrPad <ListOrPad@gmail.com>
2023-04-07 12:15:12 +0300

    generics challenge done

---
+++ Studying/CSharp Book OOP/Generics challenge(30)/ColoredItemT.cs
@@ -0,0 +1,19 @@
+﻿public class ColoredItem<T>
+{
+    public ConsoleColor Color { get; }
+    public T? Item { get; set; }
+
+    public ColoredItem(ConsoleColor color, T item)
+    {
+        Item = item;
+        Color = color;
+    }
+
+    //blue sword, green bow, red axe
+    public virtual void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item);
+    }
+
+}
---
+++ Studying/CSharp Book OOP/Generics challenge(30)/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Generics_challenge_30_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //generics "Colored Items" challenge
+
+            ColoredItem<Sword> sword = new ColoredItem<Sword>(ConsoleColor.Blue, new Sword());
+            ColoredItem<Axe> axe = new ColoredItem<Axe>(ConsoleColor.Red, new Axe());
+            ColoredItem<Bow> bow = new ColoredItem<Bow>(ConsoleColor.Green, new Bow());
+            sword.Display();
+            axe.Display();
+            bow.Display();
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
